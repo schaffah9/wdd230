e074c538d4ac19c841eca6b799e74312c8d8ecc9
schaffah9 <you@example.com>
2022-09-25 03:23:26 -0300

    fix last modified

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 let currentYear = new Date().getFullYear();
 document.querySelector(".current-year").innerHTML = currentYear;
 
-let lastModified = new Date(document.lastModified).
+let lastModified = new Date(document.lastModified);
 document.querySelector(".last-modified").innerHTML = lastModified;
