4cb2113838ef1ef067b814af3b8b1a04e08f5263
schaffah9 <you@example.com>
2022-10-14 03:36:42 -0300

    finish lesson05

---
+++ lesson05/scripts/script.js
@@ -0,0 +1,29 @@
+let currentYear = new Date().getFullYear();
+document.querySelector(".current-year").innerHTML = currentYear;
+
+let lastModified = document.lastModified;
+document.querySelector(".last-modified").innerHTML = lastModified;
+
+// W05 DOM Manipulation Activity
+
+const input = document.querySelector("input");
+const submitButton = document.querySelector("button");
+const list = document.querySelector("ul");
+
+submitButton.addEventListener("click", () => {
+	const listItem = document.createElement("li");
+	const deleteButton = document.createElement("button");
+
+	listItem.textContent = input.value;
+	deleteButton.textContent = "❌";
+
+	listItem.append(deleteButton);
+	list.append(listItem);
+
+	deleteButton.addEventListener("click", () => {
+		list.removeChild(listItem);
+	});
+
+	input.value = "";
+	input.focus();
+});
